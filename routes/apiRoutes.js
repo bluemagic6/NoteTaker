41e2021048055c7e698eb92a542f287fa88c69b9
Johnny Dickerson Jr <lilman.jdick@gmail.com>
2021-10-05 18:13:55 -0500

    added get and post route

---
+++ routes/apiRoutes.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require('path');
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
+// Add new Note
+app.post('./api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync(".db/db.json"));
+
+    const newNotes = req.body
+
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+
+    res.json(notes);
+});
