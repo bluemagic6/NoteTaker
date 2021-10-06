bff4f45d795b78bef2074b073a9d9584e7731269
Johnny Dickerson Jr <lilman.jdick@gmail.com>
2021-10-06 16:27:45 -0500

    fixed apiRoute

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,25 @@
 const fs = require("fs");
 const path = require('path');
-const express = require('express');
+// const express = require('express');
 const router = require("./htmlRoutes");
-const app = express();
+
+const uuid = require('uuid');
 
 
 router.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, "./db/db.json"));
+    res.sendFile(path.join(__dirname, "../db/db.json"));
 });
 
 // Add new Note
 router.post('./api/notes', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync(".db/db.json"));
+    const notes = JSON.parse(fs.readFileSync("../db/db.json"));
 
     const newNotes = req.body
 
     newNotes.id = uuid.v4();
     notes.push(newNotes);
 
-    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    fs.writeFileSync('../db/db.json', JSON.stringify(notes));
 
     res.json(notes);
 });
